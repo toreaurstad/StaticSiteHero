7ea8f99bfad83e0b2b6e0d83f3000ed2bb4eff59
Tore Aurstad <tore.aurstad@hemit.no>
2018-12-26 22:58:52 +0100

    Configurable paths for images and files.

--- extension.js
+++ extension.js
@@ -1,6 +1,32 @@
+"use strict";
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+
+
+let insertText = (value) => {
+	let editor = vscode.window.activeTextEditor;
+
+	if (!editor) {
+		vscode.window.showErrorMessage("Can't insert text because no document is open.");
+		return;
+	}
+	let selection = editor.selection;
+	let range = new vscode.Range(selection.start, selection.end);
+	editor.edit((editBuilder) => {
+		editBuilder.replace(range, value);
+	});
+}
+
+let getImageTemplate = () => {
+	return vscode.workspace.getConfiguration("staticSiteHero")["imagePathTemplate"];
+};
+
+let getFileTemplate = () => {
+	return vscode.workspace.getConfiguration("staticSiteHero")["filePathTemplate"];
+};
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,22 +40,41 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "static-site-hero" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let fileLinkDisposable = vscode.commands.registerCommand('extension.insertLink', () => {
+
+		let linkTypeList = ['File', 'Image'];
+
+		vscode.window.showQuickPick(linkTypeList, { placeHolder: 'Link Type' })
+			.then(result => {
+
+				if (result === 'File') {
+					insertText(getFileTemplate());
+				}
+				else if (result === 'Image') {
+					insertText(getImageTemplate());
+				}
+			});
+
+		vscode.window.showInformationMessage('Insert HTML link  initiated');
+		insertText("Insert file link");
 	});
+	context.subscriptions.push(fileLinkDisposable);
 
-	context.subscriptions.push(disposable);
+	let figureDisposable = vscode.commands.registerCommand('extension.insertFigure', () => {
+		vscode.window.showInformationMessage('Insert Figure Tag initiated');
+		insertText("Insert figure tag");
+	});
+	context.subscriptions.push(figureDisposable);
+
+
 }
+
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
